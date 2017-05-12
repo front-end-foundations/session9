38c503f6e62b6597f63873711853d1ac395fa838
DannyBoyNYC <daniel.deverell@gmail.com>
2017-05-11 21:22:31 -0400

    done for the day

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -28,9 +28,9 @@ const videoLinks = document.querySelectorAll('.content-video a')
 const videoLinksArray = [...videoLinks]
 videoLinksArray.forEach( 
 	videoLink => videoLink.addEventListener('click', selectVideo ))
-
+ 
 function selectVideo(){
-	removeActiveClass()
+	removeActiveClass('video')
 	const videoToPlay = this.getAttribute('href')
 	iFrame.setAttribute('src', videoToPlay)
 	this.classList.add('active')
@@ -38,9 +38,9 @@ function selectVideo(){
 	event.preventDefault()
 }
 
-function removeActiveClass(){
-	videoLinksArray.forEach( videoLink => videoLink.classList.remove('active'))
-}
+// function removeActiveClass(){
+// 	videoLinksArray.forEach( videoLink => videoLink.classList.remove('active'))
+// }
 
 
 
@@ -57,14 +57,185 @@ subnavLinksArray.forEach( subnavLink => subnavLink.addEventListener('click', ope
 subnavLinksArray[0].nextElementSibling.classList.add('active')
 
 function openAccordion(){
-	removeActiveClass()
+	removeActiveClass('accordion')
 	this.nextElementSibling.classList.toggle('active')
 	event.preventDefault()
 }
 
-function removeActiveClass(){
-	subnavLinksArray.forEach( subnavLink => subnavLink.nextElementSibling.classList.remove('active'))
+function removeActiveClass(locale){
+    if (locale === 'accordion') {
+        subnavLinksArray.forEach( subnavLink => subnavLink.nextElementSibling.classList.remove('active'))
+    } else if (locale === 'video') {
+        videoLinksArray.forEach( videoLink => videoLink.classList.remove('active'))
+    }
 }
+
+// $('.image-tn a').on('click tap', function(){
+//     var imgsrc = $(this).attr('href');
+//     var titleText = $(this).find('img').attr('title');
+//     $('figure > img').attr('src', imgsrc);
+//     $('figcaption').html(titleText);
+//     return false;
+// });
+
+const carouselLinks = document.querySelectorAll('.image-tn a')
+// const carouselLinksArray = [...carouselLinks]
+const carousel = document.querySelector('figure img')
+
+const carouselPara = document.querySelector('figcaption')
+
+carouselLinks.forEach(carouselLink => 
+	carouselLink.addEventListener('click', runCarousel))
+
+function runCarousel(){
+	const imageHref = this.getAttribute('href')
+	carousel.setAttribute('src', imageHref)
+	const titleText = this.firstChild.title
+	carouselPara.innerHTML = titleText
+	console.log(titleText)
+	event.preventDefault()
+}
+
+
+
+initSmoothScrolling();
+
+function initSmoothScrolling() {
+  if (isCssSmoothSCrollSupported()) {
+    return;
+  }
+
+  var duration = 400;
+
+  var pageUrl = location.hash ?
+    stripHash(location.href) :
+    location.href;
+
+  delegatedLinkHijacking();
+  //directLinkHijacking();
+
+  function delegatedLinkHijacking() {
+    document.body.addEventListener('click', onClick, false);
+
+    function onClick(e) {
+      if (!isInPageLink(e.target))
+        return;
+
+      e.stopPropagation();
+      e.preventDefault();
+
+      jump(e.target.hash, {
+        duration: duration,
+        callback: function() {
+          setFocus(e.target.hash);
+        }
+      });
+    }
+  }
+
+  function directLinkHijacking() {
+    [].slice.call(document.querySelectorAll('a'))
+      .filter(isInPageLink)
+      .forEach(function(a) {
+        a.addEventListener('click', onClick, false);
+      });
+
+    function onClick(e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      jump(e.target.hash, {
+        duration: duration,
+      });
+    }
+
+  }
+
+  function isInPageLink(n) {
+    return n.tagName.toLowerCase() === 'a' &&
+      n.hash.length > 0 &&
+      stripHash(n.href) === pageUrl;
+  }
+
+  function stripHash(url) {
+    return url.slice(0, url.lastIndexOf('#'));
+  }
+
+  function isCssSmoothSCrollSupported() {
+    return 'scrollBehavior' in document.documentElement.style;
+  }
+
+  // Adapted from:
+  // https://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
+  function setFocus(hash) {
+    var element = document.getElementById(hash.substring(1));
+
+    if (element) {
+      if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+        element.tabIndex = -1;
+      }
+
+      element.focus();
+    }
+  }
+
+}
+
+function jump(target, options) {
+  var
+    start = window.pageYOffset,
+    opt = {
+      duration: options.duration,
+      offset: options.offset || 0,
+      callback: options.callback,
+      easing: options.easing || easeInOutQuad
+    },
+    distance = typeof target === 'string' ?
+    opt.offset + document.querySelector(target).getBoundingClientRect().top :
+    target,
+    duration = typeof opt.duration === 'function' ?
+    opt.duration(distance) :
+    opt.duration,
+    timeStart, timeElapsed;
+
+  requestAnimationFrame(function(time) {
+    timeStart = time;
+    loop(time);
+  });
+
+  function loop(time) {
+    timeElapsed = time - timeStart;
+
+    window.scrollTo(0, opt.easing(timeElapsed, start, distance, duration));
+
+    if (timeElapsed < duration)
+      requestAnimationFrame(loop)
+    else
+      end();
+  }
+
+  function end() {
+    window.scrollTo(0, start + distance);
+
+    if (typeof opt.callback === 'function')
+      opt.callback();
+  }
+
+  // Robert Penner's easeInOutQuad - http://robertpenner.com/easing/
+  function easeInOutQuad(t, b, c, d) {
+    t /= d / 2
+    if (t < 1) return c / 2 * t * t + b
+    t--
+    return -c / 2 * (t * (t - 2) - 1) + b
+  }
+
+}
+
+
+
+
+
+
 
 
 
