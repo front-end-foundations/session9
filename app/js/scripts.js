ce0e087b42fd95e331f6c7b8512c7ea9935f5c29
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-13 20:42:39 -0500

    done

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,12 @@
+var log = console.log;
+
+document.addEventListener('click', (e) => {
+	log(event.target)
+	if ( e.target.id == 'pull') {
+		makeHamburgers()
+	}
+})
+
 // Functions called elsewhere
 var removeActiveClass = function (elements) {
 	document.querySelectorAll(elements).forEach( (elem) => {
@@ -13,53 +22,72 @@ var addActiveClass = function (element) {
 
 // Hamburger
 var makeHamburgers = function () {
-	var hamburger = document.querySelector('#pull');
+	event.preventDefault();
 	var body = document.querySelector('body');
-
-	hamburger.addEventListener('click', showMenu)
-
-	function showMenu() {
-		body.classList.toggle('show-nav');
-		event.preventDefault();
-	}
+	body.classList.toggle('show-nav');
 }
 
 
 // Video switcher
+
 var videoSwitcher = function () {
-	const videoLinks = Array.from(document.querySelectorAll('.content-video a'));
+	const videoLinks = document.querySelectorAll('.content-video a');
 	const iFrame = document.querySelector('iframe')
 	
-	videoLinks.forEach((videoLink) => {
+	videoLinks.forEach( (videoLink) => {
 		videoLink.addEventListener('click', selectVideo)
 	});
-		
+	
 	function selectVideo() {
+		event.preventDefault();
 		removeActiveClass('.content-video a');
 		addActiveClass(event.target)
+		// log(event.target)
 		const videoToPlay = event.target.getAttribute('href');
 		iFrame.setAttribute('src', videoToPlay);
-		event.preventDefault();
 	}
 }
-	
-	
+
+
 // Accordion
 var accordion = function () {
 	const subnavLinks = document.querySelectorAll('.nav-sub > li > a')
+	// log(subnavLinks)
 	subnavLinks[0].nextElementSibling.classList.add('active')
 	
 	subnavLinks.forEach(subnavLink => subnavLink.addEventListener('click', openAccordion))
 	
 	function openAccordion() {
+		event.preventDefault()
 		removeActiveClass('.nav-sub > li > ul');
 		addActiveClass(event.target.nextElementSibling)
-		event.preventDefault()
 	}
+}
+
+// carousel
+
+const carouselLinks = document.querySelectorAll('.image-tn a')
+const carousel = document.querySelector('figure img')
+const carouselPara = document.querySelector('figcaption')
+// log(carouselPara)
+carouselLinks.forEach( function(link) {
+	link.addEventListener('click', runCarousel)
+})
+
+function runCarousel() {
+	event.preventDefault()
+	// log(this.getAttribute('href'))
+	// log(event.target.parentElement.getAttribute('href'))
+	const imageHref = this.getAttribute('href')
+	const titleText = this.firstChild.title
+	carouselPara.innerHTML = titleText
+	log(titleText)
+	carousel.setAttribute('src', imageHref)
+	
 }
 
 // ---- Initialization ---- //
 
-makeHamburgers();
+// makeHamburgers();
 accordion();
 videoSwitcher();
